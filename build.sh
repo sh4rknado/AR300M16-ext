b0b91eac643cb2d9ebdf3461e52a7484d7e1deb2
Your Name <you@example.com>
2022-11-22 21:17:49 +0000

    update the builder

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+current_folder=$(pwd)
 
 # clean all old files
 rm ./tmp -rf
@@ -10,3 +11,10 @@ git clean -xfd
 
 cp scripts/configurations .config
 make V=s -j5
+
+# update image builder
+cd bin/targets/ar71xx/generic/
+tar xvf openwrt-imagebuilder-ar71xx-generic.Linux-x86_64.tar.xz
+cp -avr openwrt-imagebuilder-ar71xx-generic.Linux-x86_64/* $current_folder/openwrt-imagebuilder/
+rm -rfv openwrt-imagebuilder-ar71xx-generic.Linux-x86_64*
+
