46c289bfaba99b70770a316e036212d192669621
bertieaux jordan <bertieaux.jo@gmail.com>
2022-11-24 19:08:05 +0000

    clean and update builder

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-current_folder=$(pwd)
+#current_folder="$(pwd)"
 
 # clean all old files
 rm ./tmp -rf
+rm -rfv openwrt-imagebuilder
 #git clean -xfd
 
 # Making files
@@ -15,6 +16,10 @@ make V=s -j5
 # update image builder
 cd bin/targets/ar71xx/generic/
 tar xvf openwrt-imagebuilder-ar71xx-generic.Linux-x86_64.tar.xz
-cp -avr openwrt-imagebuilder-ar71xx-generic.Linux-x86_64/* $current_folder/openwrt-imagebuilder/
-rm -rfv openwrt-imagebuilder-ar71xx-generic.Linux-x86_64*
+mv openwrt-imagebuilder-ar71xx-generic.Linux-x86_64 ~/AR300M16-ext/openwrt-imagebuilder
+rm -rfv openwrt-imagebuilder-ar71xx-generic.Linux-x86_64/
 
+# copy builder
+cp -avr overlay ~/AR300M16-ext/openwrt-imagebuilder/overlay/
+mv ~/AR300M16-ext/openwrt-imagebuilder/overlay/build.sh ~/AR300M16-ext/openwrt-imagebuilder/build.sh
+mv ~/AR300M16-ext/openwrt-imagebuilder/overlay/repositories.conf ~/AR300M16-ext/openwrt-imagebuilder/repositories.conf
