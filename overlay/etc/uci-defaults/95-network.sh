6278bed8e16587f83bf4d2b760e43e25b6f026f9
bertieaux jordan <bertieaux.jo@gmail.com>
2022-11-26 08:08:48 +0000

    update default configuration

--- overlay/etc/uci-defaults/95-network.sh
+++ overlay/etc/uci-defaults/95-network.sh
@@ -1,18 +1,7 @@
-# Get Version and Device
-if [[ $(cat /proc/cpuinfo | grep 'machine' | awk {'print $6'}) == "NANO" ]]; then
-    device="NANO"
-elif [[ $(cat /proc/cpuinfo | grep 'machine' | awk {'print $6'}) == "TETRA" ]]; then
-    device="TETRA"
-fi
-
 # -- Set up Networking configuration
 uci set network.lan.type='bridge'
 uci set network.lan.proto='static'
-if [[ $device == "TETRA" ]]; then
-	uci set network.lan.ifname='eth1'
-else
-	uci set network.lan.ifname='eth0'
-fi
+uci set network.lan.ifname='eth1'
 uci set network.lan.ipaddr='172.16.42.1'
 uci set network.lan.netmask='255.255.255.0'
 uci set network.lan.gateway='172.16.42.42'
@@ -26,16 +15,11 @@ uci set network.usb.dns='8.8.8.8, 8.8.4.4'
 uci set network.wwan=interface
 uci set network.wwan.proto='dhcp'
 uci set network.wwan.dns='8.8.8.8, 8.8.4.4'
-
-if [[ $device == "TETRA" ]]; then
-	uci set network.wan.ifname='eth0'
-	uci set network.wan.proto='dhcp'
-	uci set network.wan.dns='8.8.8.8, 8.8.4.4'
-
-	uci set network.wan6.ifname='eth0'
-	uci set network.wan6.proto='dhcpv6'
-fi
+uci set network.wan.ifname='eth0'
+uci set network.wan.proto='dhcp'
+uci set network.wan.dns='8.8.8.8, 8.8.4.4'
+uci set network.wan6.ifname='eth0'
+uci set network.wan6.proto='dhcpv6'
 
 uci commit network
-
 exit 0
